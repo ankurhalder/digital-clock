f9d94755e83160b38197339e9b002ee2d7869073
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-02-01 07:23:45 +0530

    AM and PM added

--- clock.py
+++ clock.py
@@ -2,17 +2,17 @@ import tkinter as tk
 import time
 
 def update_clock():
-    current_time = time.strftime('%H:%M:%S')
+    current_time = time.strftime('%I:%M:%S %p')  # %p gives AM or PM
     clock_label.config(text=current_time)
     clock_label.after(1000, update_clock)
 
 # Create the main window
 root = tk.Tk()
 root.title("Digital Clock")
-root.geometry("200x100")
+root.geometry("250x100")
 
 # Create a label to display the clock
-clock_label = tk.Label(root, font=('calibri', 40, 'bold'), bg='black', fg='white')
+clock_label = tk.Label(root, font=('calibri', 30, 'bold'), bg='black', fg='white')
 clock_label.pack(expand=True)
 
 # Update the clock initially and start the clock update process
